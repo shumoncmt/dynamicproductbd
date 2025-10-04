112eb430e18a63a5738ceb794d4c48d35e19b054
shumoncmt <shomoncmt@gmail.com>
2025-10-04 15:27:16 +0600

    update 04.10.2025

--- resources/views/project/Deep-Tube-Well.blade.php
+++ resources/views/project/Deep-Tube-Well.blade.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Deep Tube Well in Bangladesh - Best industrial electric motor in Bangladesh | Dynamic Motor</title>
-    <meta name="description" content="Discover the benefits of deep tube wells in Bangladesh. Learn about their construction, maintenance, and impact on sustainable water supply solutions" />
-    <meta name="keywords" content="Find insights on their installation, efficiency, and contribution to local communities' water needs.">
-    
+    <meta name="description"
+        content="Discover the benefits of deep tube wells in Bangladesh. Learn about their construction, maintenance, and impact on sustainable water supply solutions" />
+    <meta name="keywords"
+        content="Find insights on their installation, efficiency, and contribution to local communities' water needs.">
+
     @include('master.header')
 
 </head>
+
 <body>
+
 
 
 
 
 </body>
 
-        
+
 
 
 
